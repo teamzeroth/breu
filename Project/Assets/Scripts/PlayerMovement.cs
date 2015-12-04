d21d53d67754220f0d636f837f0265b15a2fe955
Rafael Pinheiro <rafael.c.pinheiro@gmail.com>
2015-12-04 17:20:16 -0300

    Rodando experimentos no projeto. Passível de quebrar outras coisas! HANDLE WITH CARE

--- Project/Assets/Scripts/PlayerMovement.cs
+++ Project/Assets/Scripts/PlayerMovement.cs
@@ -21,36 +21,39 @@ public class PlayerMovement : MonoBehaviour {
     Vector3 lastVector = Vector3.zero;
     Vector3 updateVector = Vector3.zero;
 
-    FMOD_CustonEmitter _feetAudio;
+    public WallSoundCheck wallSound;
 
-    void Start() {
+    void Start()
+    {
         _rigidbody = GetComponent<Rigidbody>();
-        _feetAudio = transform.Find("Feets").GetComponent<FMOD_CustonEmitter>();
+        //_feetAudio = transform.Find("Feets").GetComponent<FMOD_CustonEmitter>();
     }
 
-    void Update() {
-        Vector3 vector = usingTouch ? getTouchAxis() : getAxis();
+    void Update()
+    {
+        Vector3 vector = usingTouch ? GetTouchAxis() : GetAxis();
 
         if (vector == Vector3.zero) {
             lastVector = updateVector;
-            playerFriction();
+            PlayerFriction();
 
-            if (_feetAudio.HasPlayed()) {
+            if (wallSound.HasPlayed()) {
                 //_feetAudio.SetParameter("velocidade", 0);
-                _feetAudio.Stop(0.1f);
+                wallSound.Stop(0.1f);
             }
 
         } else {
-            if (!_feetAudio.HasPlayed() && !_feetAudio.HasStarted()) {
-                _feetAudio.SetParameter("velocidade", 0);
-                _feetAudio.Play();
+            if (!wallSound.HasPlayed() && !wallSound.HasStarted()) {
+                wallSound.SetParameter("velocidade", 0);
+                wallSound.Play();
             }
 
-            playerInput(vector);
+            PlayerInput(vector);
         }
     }
 
-    Vector3 getAxis() {
+    Vector3 GetAxis()
+    {
         Vector3 temp = new Vector3(
             Input.GetAxis("Horizontal"),
             0,
@@ -61,7 +64,9 @@ public class PlayerMovement : MonoBehaviour {
     }
 
     Vector2 initialTouchPos;
-    Vector3 getTouchAxis() {
+
+    Vector3 GetTouchAxis()
+    {
         if (Input.touchCount != 0) {
             if (initialTouchPos == Vector2.zero)
                 initialTouchPos = Input.GetTouch(0).position;
@@ -74,14 +79,15 @@ public class PlayerMovement : MonoBehaviour {
         return Vector3.zero;
     }
 
-    void playerInput(Vector3 vector) {
+    void PlayerInput(Vector3 vector)
+    {
         float oldAngle = Mathf.Atan2(lastVector.x, lastVector.z);
         float curAngle = Mathf.Atan2(vector.x, vector.z);
 
         float angle = oldAngle + curAngle;
         float force = Mathf.Min(vector.magnitude, maxTouchForce) * speed;
 
-        _feetAudio.SetParameter("velocidade", Mathf.Min(vector.magnitude, 0.8f));
+        wallSound.SetParameter("velocidade", Mathf.Min(vector.magnitude, 0.8f));
 
         Vector3 rotation = new Vector3(0, angle * Mathf.Rad2Deg, 0);
 
@@ -91,20 +97,21 @@ public class PlayerMovement : MonoBehaviour {
             transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(rotation), Time.deltaTime * spin);
     }
 
-    void playerFriction() {
+    void PlayerFriction()
+    {
         _rigidbody.velocity = Vector3.Lerp(_rigidbody.velocity, Vector3.zero, Time.deltaTime * friction);
     }
 
-    public void OnDrawGizmos() {
 #if UNITY_EDITOR
+    public void OnDrawGizmos()
+    {
         Handles.color = Color.green;
         Handles.ArrowCap(0,
             transform.position,
             transform.rotation,
             1
         );
-#endif
     }
-
+#endif
 
 }
---
+++ Project/Assets/Scripts/WallSoundCheck.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class WallSoundCheck : MonoBehaviour {
+
+    public FMOD_CustonEmitter feetEmitter;
+    public FMOD_CustonEmitter eastEmitter;
+    public FMOD_CustonEmitter westEmitter;
+    public FMOD_CustonEmitter northEmitter;
+    public FMOD_CustonEmitter southEmitter;
+
+    public float raycastRange = 5.0f;
+
+    public float speedOfSound = 340.29f;
+
+    private Transform myTransform;
+    private bool isPlaying = false;
+
+    void Awake()
+    {
+        myTransform = transform;
+    }
+
+    public void Play()
+    {
+        isPlaying = true;
+        feetEmitter.Play();
+
+        eastEmitter.Play();
+        westEmitter.Play();
+        northEmitter.Play();
+        southEmitter.Play();
+
+
+        StartCoroutine(TrackWall());
+    }
+
+    private IEnumerator TrackWall()
+    {
+        while(isPlaying)
+        {
+            Vector3 myPosition = myTransform.position;
+            WallRaycast(Vector3.forward, northEmitter, myPosition);
+            WallRaycast(Vector3.back, southEmitter, myPosition);
+            WallRaycast(Vector3.right, eastEmitter, myPosition);
+            WallRaycast(Vector3.left, westEmitter, myPosition);
+            yield return null;
+        }
+    }
+
+    private void WallRaycast(Vector3 direction, FMOD_CustonEmitter emitter, Vector3 myPosition)
+    {
+        Transform emitterTransform = emitter.transform;
+
+        RaycastHit hit;
+        if (Physics.Raycast(myPosition, direction, out hit, raycastRange))
+        {
+            emitterTransform.position = hit.point;// + 10.0f * direction;
+            //if(emitter.HasStoped())
+            //    emitter.Play();
+        }
+        else
+        {
+            emitterTransform.position = new Vector3(10000,10000,10000);
+            //emitter.Stop();
+        }
+    }
+
+    IEnumerator PlayDelayedSound(FMOD_CustonEmitter emitter, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        emitter.Play();
+    }
+
+    public bool HasPlayed()
+    {
+        return feetEmitter.HasPlayed();
+    }
+
+    public bool HasStarted()
+    {
+        return feetEmitter.HasStarted();
+    }
+
+    public void SetParameter(string parameter, float value)
+    {
+        feetEmitter.SetParameter(parameter, value);
+        eastEmitter.SetParameter(parameter, value);
+        westEmitter.SetParameter(parameter, value);
+        northEmitter.SetParameter(parameter, value);
+        southEmitter.SetParameter(parameter, value);
+    }
+
+    public void Stop(float delay)
+    {
+        isPlaying = false;
+        feetEmitter.Stop(delay);
+        eastEmitter.Stop(delay);
+        westEmitter.Stop(delay);
+        northEmitter.Stop(delay);
+        southEmitter.Stop(delay);
+    }
+
+#if UNITY_EDITOR
+    public void OnDrawGizmos()
+    {
+        Handles.color = Color.red;
+        Vector3 pos = transform.position;
+        Handles.DrawLine(pos, pos + Vector3.forward * raycastRange);
+        Handles.DrawLine(pos, pos + Vector3.back * raycastRange);
+        Handles.DrawLine(pos, pos + Vector3.right * raycastRange);
+        Handles.DrawLine(pos, pos + Vector3.left * raycastRange);
+    }
+#endif
+}
